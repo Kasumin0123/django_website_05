2a0d4f9da22eeabee1afd2f6850e5884e9a221ad
Shunsuke Aoki <aoki.s.aj@m.titech.ac.jp>
2023-05-31 07:34:46 +0900

    Modify settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,12 +81,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-import dj_database_url
-from dotenv import (find_dotenv, load_dotenv)
-load_dotenv(find_dotenv())
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sample-postgresql',
+        'USER': 'db_django_user',
+        'PASSWORD': 'KXLeRwcPugfp9wbEr8xVX2EA9E46sMX4',
+        'HOST': 'dpg-chqvkpm4dad3eommue3g-a',
+        'PORT': '5432',
+     }
 }
+
+# import dj_database_url
+# from dotenv import (find_dotenv, load_dotenv)
+# load_dotenv(find_dotenv())
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600),
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
