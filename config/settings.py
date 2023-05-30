07b94b5bac89810ac37c201f6a789217bcbaabfe
Shunsuke Aoki <aoki.s.aj@m.titech.ac.jp>
2023-05-31 08:07:33 +0900

    Test database

--- config/settings.py
+++ config/settings.py
@@ -81,23 +81,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_django',
-        'USER': 'db_django_user',
-        'PASSWORD': 'KXLeRwcPugfp9wbEr8xVX2EA9E46sMX4',
-        'HOST': 'dpg-chqvkpm4dad3eommue3g-a',
-        'PORT': '5432',
-     }
-}
-
-# import dj_database_url
-# from dotenv import (find_dotenv, load_dotenv)
-# load_dotenv(find_dotenv())
+# 本番環境（Render）用
 # DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600),
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_django',
+#         'USER': 'db_django_user',
+#         'PASSWORD': 'KXLeRwcPugfp9wbEr8xVX2EA9E46sMX4',
+#         'HOST': 'dpg-chqvkpm4dad3eommue3g-a',
+#         'PORT': '5432',
+#      }
 # }
+
+# 開発環境用
+import dj_database_url
+from dotenv import (find_dotenv, load_dotenv)
+load_dotenv(find_dotenv())
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
